74fe530b34a504452857b60022eeaef247d33526
dutchenkoOleg <dutchenko.o.dev@gmail.com>
2020-12-13 00:19:45 +0200

    Add `isAndroidVersion` test coverage

--- src/__specs__/detect/os/android-version.spec.ts
+++ src/__specs__/detect/os/android-version.spec.ts
@@ -1,7 +1,7 @@
 import browserizr from '../../../core';
 import { isAndroidOSVersion } from '../../../detect/os/android-version';
 import { uaDB } from '../../db';
-import { MORE_THEN_OR_EQUAL } from '../../../utils';
+import { EQUAL, LESS_THEN_OR_EQUAL, MORE_THEN_OR_EQUAL } from '../../../utils';
 
 describe('Detect Android OS Version', () => {
 	describe('Android 10', () => {
@@ -22,6 +22,124 @@ describe('Detect Android OS Version', () => {
 					browserizr.setUA(ua);
 					expect(
 						browserizr.detect(isAndroidOSVersion(MORE_THEN_OR_EQUAL, 10))
+					).toBeTruthy();
+				});
+			});
+		});
+
+		describe(`Should be ${EQUAL}`, () => {
+			[
+				...uaDB.Android_10.ChromeAndroid_85.Standard,
+				...uaDB.Android_10.ChromeAndroid_87.Standard,
+				...uaDB.Android_10.ChromeAndroid_87['Chrome on Lg'],
+				...uaDB.Android_10.ChromeAndroid_87['Chrome on Samsung'],
+				...uaDB.Android_10.EdgeAndroid_45.Standard,
+				...uaDB.Android_10.Opera_59['Opera on Huawei'],
+				...uaDB.Android_10.Opera_59['Opera on Sumsung']
+			].forEach((ua, i) => {
+				test(`Case #${++i}: ${ua}`, () => {
+					browserizr.setUA(ua);
+					expect(browserizr.detect(isAndroidOSVersion(EQUAL, 10))).toBeTruthy();
+				});
+			});
+		});
+
+		describe(`Should be ${LESS_THEN_OR_EQUAL}`, () => {
+			[
+				...uaDB.Android_10.ChromeAndroid_85.Standard,
+				...uaDB.Android_10.ChromeAndroid_87.Standard,
+				...uaDB.Android_10.ChromeAndroid_87['Chrome on Lg'],
+				...uaDB.Android_10.ChromeAndroid_87['Chrome on Samsung'],
+				...uaDB.Android_10.EdgeAndroid_45.Standard,
+				...uaDB.Android_10.Opera_59['Opera on Huawei'],
+				...uaDB.Android_10.Opera_59['Opera on Sumsung']
+			].forEach((ua, i) => {
+				test(`Case #${++i}: ${ua}`, () => {
+					browserizr.setUA(ua);
+					expect(
+						browserizr.detect(isAndroidOSVersion(LESS_THEN_OR_EQUAL, 10))
+					).toBeTruthy();
+				});
+			});
+		});
+
+		describe(`Should not be ${LESS_THEN_OR_EQUAL}`, () => {
+			[
+				...uaDB.Android_11.FirefoxAndroid_83.Standard,
+				...uaDB.Android_11.FirefoxAndroid_83['Firefox on Lg'],
+				...uaDB.Android_11.Yandex_20.Standard
+			].forEach((ua, i) => {
+				test(`Case #${++i}: ${ua}`, () => {
+					browserizr.setUA(ua);
+					expect(
+						browserizr.detect(isAndroidOSVersion(LESS_THEN_OR_EQUAL, 10))
+					).toBeFalsy();
+				});
+			});
+		});
+
+		describe(`Should not pass at all`, () => {
+			[
+				...uaDB.Linux.Chrome_87.Standard,
+				...uaDB.MacOS_11.Safari_14.Standard,
+				...uaDB.Windows_10.Chrome_87.Standard
+			].forEach((ua, i) => {
+				test(`Case #${++i}: ${ua}`, () => {
+					browserizr.setUA(ua);
+					expect(
+						browserizr.detect(isAndroidOSVersion(LESS_THEN_OR_EQUAL, 10))
+					).toBeFalsy();
+				});
+			});
+		});
+	});
+
+	describe('Android 11', () => {
+		describe(`Should be ${MORE_THEN_OR_EQUAL}`, () => {
+			[
+				...uaDB.Android_11.FirefoxAndroid_83.Standard,
+				...uaDB.Android_11.FirefoxAndroid_83['Firefox on Lg'],
+				...uaDB.Android_11.Yandex_20.Standard
+			].forEach((ua, i) => {
+				test(`Case #${++i}: ${ua}`, () => {
+					browserizr.setUA(ua);
+					expect(
+						browserizr.detect(isAndroidOSVersion(MORE_THEN_OR_EQUAL, 11))
+					).toBeTruthy();
+				});
+			});
+		});
+
+		describe(`Should be ${EQUAL}`, () => {
+			[
+				...uaDB.Android_11.FirefoxAndroid_83.Standard,
+				...uaDB.Android_11.FirefoxAndroid_83['Firefox on Lg'],
+				...uaDB.Android_11.Yandex_20.Standard
+			].forEach((ua, i) => {
+				test(`Case #${++i}: ${ua}`, () => {
+					browserizr.setUA(ua);
+					expect(browserizr.detect(isAndroidOSVersion(EQUAL, 11))).toBeTruthy();
+				});
+			});
+		});
+
+		describe(`Should be ${LESS_THEN_OR_EQUAL}`, () => {
+			[
+				...uaDB.Android_10.ChromeAndroid_85.Standard,
+				...uaDB.Android_10.ChromeAndroid_87.Standard,
+				...uaDB.Android_10.ChromeAndroid_87['Chrome on Lg'],
+				...uaDB.Android_10.ChromeAndroid_87['Chrome on Samsung'],
+				...uaDB.Android_10.EdgeAndroid_45.Standard,
+				...uaDB.Android_10.Opera_59['Opera on Huawei'],
+				...uaDB.Android_10.Opera_59['Opera on Sumsung'],
+				...uaDB.Android_11.FirefoxAndroid_83.Standard,
+				...uaDB.Android_11.FirefoxAndroid_83['Firefox on Lg'],
+				...uaDB.Android_11.Yandex_20.Standard
+			].forEach((ua, i) => {
+				test(`Case #${++i}: ${ua}`, () => {
+					browserizr.setUA(ua);
+					expect(
+						browserizr.detect(isAndroidOSVersion(LESS_THEN_OR_EQUAL, 11))
 					).toBeTruthy();
 				});
 			});
