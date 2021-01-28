046f50e77e16fc0cd982ad58124bbd9230a64590
dutchenkoOleg <dutchenko.o.dev@gmail.com>
2021-01-29 00:03:33 +0200

    DB: Extend IE11

--- src/__specs__/db.ts
+++ src/__specs__/db.ts
@@ -767,7 +767,8 @@ export const uaDB = {
 				v11: {
 					Standard: [
 						'Mozilla/5.0 (Windows NT 10.0; Trident/7.0; rv:11.0) like Gecko',
-						'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; Zoom 3.6.0; rv:11.0) like Gecko'
+						'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; Zoom 3.6.0; rv:11.0) like Gecko',
+						'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; rv:11.0) like Gecko'
 					]
 				}
 			},
